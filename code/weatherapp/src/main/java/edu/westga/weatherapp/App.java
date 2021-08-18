8faa4ae16d613756682f0e5cb6fd4b2961517bb4
Thomas Morgenstern <tmorgen1@my.westga.edu>
2021-08-18 17:47:38 -0400

    Initial weather app project created Initial weather app object created, should be able to run mvn compile or mvn package on this project.  .gitignore has been edited to not include VSCode personal temp file elements in commits.

---
+++ code/weatherapp/src/main/java/edu/westga/weatherapp/App.java
@@ -0,0 +1,13 @@
+package edu.westga.weatherapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
