0a599c0acfa1b85ea03b21eb6183521656e13f36
Eboni Walker <ebonikwalker@gmail.com>
2021-09-15 16:35:52 -0400

    Created model and tests for Severe Weather Warnings Created the OpenWeatherSevereWarningsRetriever class that implements the SevereWeatherWarningsRetriever interface. Created the SevereWeatherWarning class to wrap the retrieved warning data. Added Tests for both the OpenWeatherSevereWarningsRetriever and SevereWeatherWarning classes. Added the ServiceConstants class to hold all the constants that are shared between classes in the service. Added the Jacoco plugin to the service pom file to generate code coverage reports. Added Json dependency to the service pom file to use in the OpenWeatherSevereWarningsRetriever class.

---
+++ code/weatherapp_service/src/main/java/edu/westga/weatherapp_service/model/OpenWeatherSevereWarningsRetriever.java
@@ -0,0 +1,99 @@
+package edu.westga.weatherapp_service.model;
+
+import edu.westga.weatherapp_service.enums.MeasurementUnits;
+import edu.westga.weatherapp_service.interfaces.DataRetriever;
+import edu.westga.weatherapp_service.resources.ServiceConstants;
+import edu.westga.weatherapp_shared.SevereWeatherWarningsRetriever;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * This severe warnings retriever uses the OpenWeather API to obtain weather
+ * icons. Also extends the UnicastRemoteObject to allow for Remote Method
+ * Invocation.
+ * 
+ * @author Eboni Walker
+ * @version 1.0
+ */
+public class OpenWeatherSevereWarningsRetriever extends UnicastRemoteObject implements SevereWeatherWarningsRetriever {
+
+    public static final int LATITUDE_UPPER_BOUND = 90;
+    public static final int LATITUDE_LOWER_BOUND = -90;
+    public static final int LONGITUDE_UPPER_BOUND = 180;
+    public static final int LONGITUDE_LOWER_BOUND = -180;
+    private static final String SEVERE_WARNING_BASE_URL = "http:/"
+            + "/pro.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly,daily,current";
+    private DataRetriever dataRetriever;
+
+    /**
+     * Creates an Open Weather Severe Warnings Retriever
+     * 
+     * @param dataRetriever the data retriever
+     * 
+     * @precondition dataRetriever != null
+     * @postcondition none
+     * @throws RemoteException
+     */
+    public OpenWeatherSevereWarningsRetriever(DataRetriever dataRetriever) throws RemoteException {
+        super();
+        if (dataRetriever == null) {
+            throw new IllegalArgumentException("The retriever cannot be null");
+        }
+        this.dataRetriever = dataRetriever;
+    }
+
+    @Override
+    public SevereWeatherWarning[] getSevereWeatherWarningsForLocation(double latitude, double longitude, Enum<?> units)
+            throws RemoteException, IllegalArgumentException {
+        if (latitude < OpenWeatherSevereWarningsRetriever.LATITUDE_LOWER_BOUND
+                || latitude > OpenWeatherSevereWarningsRetriever.LATITUDE_UPPER_BOUND) {
+            throw new IllegalArgumentException(ServiceConstants.LATITUDE_OUT_OF_BOUND);
+        }
+        if (longitude < OpenWeatherSevereWarningsRetriever.LONGITUDE_LOWER_BOUND
+                || longitude > OpenWeatherSevereWarningsRetriever.LONGITUDE_UPPER_BOUND) {
+            throw new IllegalArgumentException(ServiceConstants.LONGITUDE_OUT_OF_BOUND);
+        }
+        if (units == null || !units.getClass().equals(MeasurementUnits.class)) {
+            throw new IllegalArgumentException(ServiceConstants.INCORRECT_UNITS);
+        }
+        JSONObject data = new JSONObject(this.fetchDataOfSevereWeatherWarningsForLocation(latitude, longitude, units));
+        SevereWeatherWarning[] severeWeatherWarnings = this.convertSevereWeatherWarningsForLocationFromJsonToArray(data);
+        return severeWeatherWarnings;
+    }
+
+    private SevereWeatherWarning[] convertSevereWeatherWarningsForLocationFromJsonToArray(JSONObject data) {
+        JSONArray warningsData = this.parseDataFromJsonObjectAsAJsonArray(data);
+        SevereWeatherWarning[] severeWeatherWarnings = new SevereWeatherWarning[warningsData.length()];
+        for (int ix = 0; ix < warningsData.length(); ix++) {
+            String forecasterName = warningsData.getJSONObject(ix).getString("sender_name");
+            String warningName = warningsData.getJSONObject(ix).getString("event");
+            String start = String.valueOf(warningsData.getJSONObject(ix).getLong("start"));
+            String end = String.valueOf(warningsData.getJSONObject(ix).getLong("end"));
+            String description = warningsData.getJSONObject(ix).getString("description");
+            severeWeatherWarnings[ix] = new SevereWeatherWarning(forecasterName, warningName, start, end, description);
+        }
+        return severeWeatherWarnings;
+    }
+
+    private String fetchDataOfSevereWeatherWarningsForLocation(double latitude, double longitude, Enum<?> units) {
+        MeasurementUnits unit = Enum.valueOf(MeasurementUnits.class, units.name());
+        String paramLatitude = "&lat=" + latitude;
+        String paramLongitude = "&lon=" + longitude;
+        URL apiCall = this.dataRetriever.GetServiceAPICallURL(paramLatitude + paramLongitude,
+                OpenWeatherSevereWarningsRetriever.SEVERE_WARNING_BASE_URL, ServiceConstants.API_KEY, unit);
+        return this.dataRetriever.GetData(apiCall);
+    }
+
+    private JSONArray parseDataFromJsonObjectAsAJsonArray(JSONObject data) {
+        JSONArray warningsData = data.optJSONArray("alerts");
+        if (warningsData == null) {
+            warningsData = new JSONArray();
+        }
+        return warningsData;
+    }
+}
---
+++ code/weatherapp_shared/src/main/java/edu/westga/weatherapp_shared/SevereWeatherWarningsRetriever.java
@@ -0,0 +1,31 @@
+package edu.westga.weatherapp_shared;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Severe Weather Warnings Retriever interface. Extends the remote class to
+ * allow for Remote Method Invocation. Objects that implement this interface
+ * should be able to obtain the severe weather warnings for a given location.
+ * 
+ * @author Eboni Walker
+ * @version 1.0
+ */
+public interface SevereWeatherWarningsRetriever extends Remote {
+
+    /**
+     * Gets an array of the severe weather warnings for the given location
+     * 
+     * @param latitude  the latitude of the location
+     * @param longitude the longitude of the location
+     * @param unit      the temperature unit to get data in
+     * 
+     * @precondition -90 >= latitude <= 90 && -180 >= longitude <= 180 && unit.class.equals(MeasurementUnits.class)
+     * @postconditon none
+     * 
+     * @return a string array of the severe weather warnings for the given location
+     * @throws RemoteException
+     */
+    Object[] getSevereWeatherWarningsForLocation(double latitude, double longitude,
+            Enum<?> unit) throws RemoteException, IllegalArgumentException;
+}
