99827a0533844fcacba2540f1ee644308ce6ee50
Thomas Morgenstern <tmorgen1@my.westga.edu>
2021-10-26 20:06:42 -0400

    Attempt to fix an error with CI-Checks and Testing.
    Edited an import.

--- code/weatherapp_gui/src/test/java/edu/westga/weatherapp_gui/SevereWeatherWarningPageViewModelTests.java
+++ code/weatherapp_gui/src/test/java/edu/westga/weatherapp_gui/SevereWeatherWarningPageViewModelTests.java
@@ -1,10 +1,10 @@
 package edu.westga.weatherapp_gui;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
