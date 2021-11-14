d2e8e3cdea97b37100fd54cd6bdbc8434fbee2ad
Michael Pavich <mpavich2@my.westga.edu>
2021-11-13 20:49:14 -0500

    Added attempted fix for CI-Check
    Changed how it moves the mouse to select things to be relative to the fxid rather than moving by pixels.
    
    Added some more print statements to debug what is happening in the CI-Check.

--- code/weatherapp_gui/src/test/java/edu/westga/weatherapp_gui/view/weather_warnings_page/WeatherWarningViewTests.java
+++ code/weatherapp_gui/src/test/java/edu/westga/weatherapp_gui/view/weather_warnings_page/WeatherWarningViewTests.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
 import edu.westga.weatherapp_gui.view.ViewTestWithServer;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 
@@ -20,10 +22,11 @@ public class WeatherWarningViewTests extends ViewTestWithServer {
         }
         this.doubleClickOn("#locationSearchTextField");
         this.type(KeyCode.S, KeyCode.E, KeyCode.A, KeyCode.T, KeyCode.T, KeyCode.L, KeyCode.E);
-        sleep(6000);
-        this.moveTo("#searchResultsListView");
-        this.moveBy(0, -70);
-        this.press(MouseButton.PRIMARY);
+        sleep(20000);
+        Point2D searchResultsPoint = this.lookup("#searchResultsListView").tryQuery().get().localToScreen(5, 5);
+        this.clickOn(searchResultsPoint, MouseButton.PRIMARY);
+        TextField locationSearchTextField = (TextField) this.lookup("#locationSearchTextField").tryQuery().get();
+        System.out.println(locationSearchTextField.getText());
         while (progressIndicator.get().isVisible())
         {
             sleep(10);
@@ -31,8 +34,8 @@ public class WeatherWarningViewTests extends ViewTestWithServer {
         this.doubleClickOn("#warningsButton");
         sleep(4000);
         this.clickOn("#severeWarningsComboBox");
-        this.moveBy(0, 40);
-        this.press(MouseButton.PRIMARY);
+        Point2D severeWarningsPoint = this.lookup("#severeWarningsComboBox").tryQuery().get().localToScreen(20, 50);
+        this.clickOn(severeWarningsPoint, MouseButton.PRIMARY);
         this.clickOn("#backArrowImageView");
         this.doubleClickOn("#backArrowImageView");
         sleep(2000);
